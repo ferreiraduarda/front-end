794546b9ad7518a4698e1784d7744ade6840e480
ferreiraduarda <maria.e.santos432@aluno.senai.br>
2026-04-13 16:52:50 -0300

    criando uma classe

---
+++ classes/encapsulamento.js
@@ -0,0 +1,10 @@
+class Carro {
+    constructor(nome, ano){
+        this.nome = nome;
+        this.ano = ano;
+    }
+}
+
+const meuCarro = new Carro("Opala", 1972);
+
+console.log(meuCarro.nome);
